91ee12f5f7bd5c9a7f299d9aeca77e149d901ff5
Suhaas Korampally <korampallysuhaas@gmail.com>
2021-06-27 16:06:59 +0530

    Add files via upload

---
+++ public/javascripts/ls.js
@@ -0,0 +1,46 @@
+const ls = {
+    saveUserInLocalStorage: function ( userJson )
+    {
+        window.localStorage.setItem( 'currentUser', JSON.stringify( userJson ) );
+    },
+    removeCurrentUser: function ()
+    {
+        window.localStorage.removeItem( 'currentUser' );
+    },
+    getCurrentUser: function ()
+    {
+        return window.localStorage.getItem( 'currentUser' );
+    },
+    getCurrentUserEmail: function ()
+    {
+        var curUserString = this.getCurrentUser();
+        if ( curUserString )
+        {
+            var json = JSON.parse( curUserString );
+            if ( json && json.email )
+                return json.email;
+            return "";
+        }
+        return "";
+    },
+    getCurrentUserId: function ()
+    {
+        var curUserString = this.getCurrentUser();
+        if ( curUserString )
+        {
+            var json = JSON.parse( curUserString );
+            if ( json && json._id )
+                return json._id;
+            return "";
+        }
+        return "";
+    },
+    isUserLoggedIn: function ()
+    {
+        if ( this.getCurrentUser() == null )
+            return false;
+        return true;
+    }
+};
+
+export default ls;
